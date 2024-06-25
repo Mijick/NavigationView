72be1177662e1ea20930e70d1e4eb89cef9281b0
Tomasz Kurylik <fulcrumone@icloud.com>
2024-06-25 16:43:35 +0200

    1

--- Sources/Internal/Views/NavigationView.swift
+++ Sources/Internal/Views/NavigationView.swift
@@ -23,8 +23,9 @@ struct NavigationView: View {
     var body: some View {
         ZStack { ForEach(temporaryViews, id: \.id, content: createItem) }
             .ignoresSafeArea(.container)
-            .gesture(createDragGesture())
+            .highPriorityGesture(createDragGesture())
             .onChange(of: stack.views, perform: onViewsChanged)
+            .onChange(of: isGestureActive, perform: onDragGestureEnded)
             .onAnimationCompleted(for: animatableData.opacity, perform: onAnimationCompleted)
     }
 }
