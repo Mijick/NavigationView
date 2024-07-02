eb676ff4377eec97af0b87b2729c2cb2575cbd24
Tomasz Kurylik <fulcrumone@icloud.com>
2024-07-02 18:14:24 +0200

    1

--- Sources/Internal/Managers/KeyboardManager.swift
+++ Sources/Internal/Managers/KeyboardManager.swift
@@ -1,5 +1,5 @@
 //
-//  KeyboardManager.swift of 
+//  KeyboardManager.swift of NavigationView
 //
 //  Created by Tomasz Kurylik
 //    - Twitter: https://twitter.com/tkurylik
@@ -9,4 +9,36 @@
 //  Copyright ©2024 Mijick. Licensed under MIT License.
 
 
-import Foundation
+import SwiftUI
+import Combine
+
+class KeyboardManager: ObservableObject {
+    @Published private(set) var isActive: Bool = false
+    private var subscription: [AnyCancellable] = []
+
+    static let shared: KeyboardManager = .init()
+    private init() { subscribeToKeyboardEvents() }
+}
+
+// MARK: - Hiding Keyboard
+extension KeyboardManager {
+    static func hideKeyboard() { UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil) }
+}
+
+// MARK: - Show / Hide Events
+private extension KeyboardManager {
+    func subscribeToKeyboardEvents() { Publishers.Merge(keyboardWillOpenPublisher, keyboardWillHidePublisher)
+        .sink { self.isActive = $0 }
+        .store(in: &subscription)
+    }
+}
+private extension KeyboardManager {
+    var keyboardWillOpenPublisher: Publishers.Map<NotificationCenter.Publisher, Bool> { NotificationCenter.default
+        .publisher(for: UIResponder.keyboardWillShowNotification)
+        .map { _ in true }
+    }
+    var keyboardWillHidePublisher: Publishers.Map<NotificationCenter.Publisher, Bool> { NotificationCenter.default
+        .publisher(for: UIResponder.keyboardWillHideNotification)
+        .map { _ in false }
+    }
+}
